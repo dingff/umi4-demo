e6b6130c63076d4287db963dd26cb4a3e4453174
dingff <@521125z>
2022-09-29 14:34:46 +0800

    feat: :sparkles: init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,16 @@
+import yayJpg from '@/assets/yay.jpg';
+import styles from './index.scss'
+
+export default function HomePage() {
+  return (
+    <div className={styles.title}>
+      <h2>Yay! Welcome to umi!</h2>
+      <p>
+        <img src={yayJpg} width="388" />
+      </p>
+      <p>
+        To get started, edit <code>pages/index.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+}
