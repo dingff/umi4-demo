43bd324ffadd808ab29828f907e48c5a5855444f
dingff <@521125z>
2022-09-30 09:43:32 +0800

    feat: :sparkles: 引入@alifd/next

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,8 @@
 import './index.scss'
+import moment from 'moment';
+import 'moment/locale/zh-cn'
 
+moment.locale('zh-cn');
 export default function Layout(props: any) {
   return props.children
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,24 @@
 import yayJpg from '@/assets/yay.jpg';
+import { Button, Tab, DatePicker } from '@alifd/next';
 import styles from './index.scss'
 
 export default function HomePage() {
   return (
     <div className={styles.title}>
       <h2>Yay! Welcome to umi!</h2>
+      <Button type='primary'>23</Button>
+      <DatePicker />
+      <Tab>
+        <Tab.Item title="Home" key="1">
+          Home content
+        </Tab.Item>
+        <Tab.Item title="Documentation" key="2">
+          Doc content
+        </Tab.Item>
+        <Tab.Item title="Help" key="3">
+          Help Content
+        </Tab.Item>
+      </Tab>,
       <div className='big'>哈哈哈哈哈</div>
       <p>
         <img src={yayJpg} width="388" />
